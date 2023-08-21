117bb92a240db052ea90f4444b34dd6670bcce38
b4yerl <gcbayerlc@gmail.com>
2023-08-21 08:28:52 -0600

    Arquétipo Maven inicializado
    - Foi utilizado o maven-archetype-quickstart
    - Adicionada dependencia Apache Commons IO

---
+++ duplicate-killer/src/main/java/com/github/b4yerl/App.java
@@ -0,0 +1,13 @@
+package com.github.b4yerl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
